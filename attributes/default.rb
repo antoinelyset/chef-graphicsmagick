0213eb4016bec1054c2042f28d6883a02c801cbe
Antoine Lyset <antoinelyset+github@gmail.com>
2013-09-05 15:55:25 +0200

    Fix double '//' in Url

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,5 +5,5 @@
 
 # GraphicsMagick Attributes
 default['graphicsmagick']['version']	= '1.3.18'
-default['graphicsmagick']['url']     	= 'http://78.108.103.11/MIRROR/ftp/GraphicsMagick/'
+default['graphicsmagick']['url']     	= 'http://78.108.103.11/MIRROR/ftp/GraphicsMagick'
 
