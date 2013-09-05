195e1cef43ddce39cb6a756d738dbf63effbdd6e
Antoine Lyset <antoinelyset+github@gmail.com>
2013-09-05 17:05:39 +0200

    Reformat Space

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,6 +4,6 @@
 #
 
 # GraphicsMagick Attributes
-default['graphicsmagick']['version']	= '1.3.18'
-default['graphicsmagick']['url']     	= 'http://78.108.103.11/MIRROR/ftp/GraphicsMagick'
+default['graphicsmagick']['version']  = '1.3.18'
+default['graphicsmagick']['url']      = 'http://78.108.103.11/MIRROR/ftp/GraphicsMagick'
 
